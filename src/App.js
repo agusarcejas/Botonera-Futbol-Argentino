28199035364db7e5293a2a4e79f76fc4de6f662b
agus75 <agus782009@gmail.com>
2023-03-08 17:34:25 -0300

    Modificacion Movil

--- src/App.js
+++ src/App.js
@@ -9,16 +9,18 @@ import BotonEscudo from './componentes/BotonEscudo';
 function App() {
 
   const [selectedButton, setSelectedButton] = useState('');
+  
+
 
   function manejarClick(buttonId) {
-    setSelectedButton(buttonId);
+    setSelectedButton(buttonId); //Asigna al hook el el boton seleccionado
   }
 
   return (
     <div className="App">
       <h1> BOTONERA DEL FUTBOL ARGENTINO </h1>
       <div className='contenedor-copa-botonera-escudo'>
-        
+
         <div className='contenedor-copa-no-movil'>
           <BotonCopa />
         </div>
@@ -30,42 +32,39 @@ function App() {
               id={'boton1'}
               imagen='anda-para-alla-bobo.jpg'
               audio='anda-para-alla-bobo-audio.mp3'
-              nombre='Anda pa´ alla bobo'
               estaApretado={selectedButton === 'boton1'}
-              manejarClick={manejarClick} />
+              manejarClick={manejarClick}
+               />
             <Boton
               id={'boton2'}
               imagen='its-very-difficult.jpg'
               audio='very-difficult-audio.mp3'
-              nombre='Its very difficult'
               estaApretado={selectedButton === 'boton2'}
-              manejarClick={manejarClick} />
+              manejarClick={manejarClick}
+               />
             <Boton
               id={'boton3'}
               imagen='me-cortaron-las-piernas.jpg'
               audio='me-cortaron-las-piernas-audio.mp3'
-              nombre='Me cortaron las piernas'
               estaApretado={selectedButton === 'boton3'}
-              manejarClick={manejarClick} />
+              manejarClick={manejarClick}
+               />
             <Boton
               id={'boton4'}
               imagen='passman-la-tenes-adentro.jpg'
               audio='passman-la-tenes-adentro.mp3'
-              nombre='Passman LTA'
               estaApretado={selectedButton === 'boton4'}
               manejarClick={manejarClick} />
             <Boton
               id={'boton5'}
               imagen='es-gatorade-señorita.jpg'
               audio='es-gatorade-señorita.mp3'
-              nombre='Es Gatorade señorita'
               estaApretado={selectedButton === 'boton5'}
               manejarClick={manejarClick} />
             <Boton
               id={'boton6'}
               imagen='gol-maradona-inglaterra.jpg'
               audio='gol-maradona-inglaterra.mp3'
-              nombre='Gol de Maradona vs Inglaterra'
               estaApretado={selectedButton === 'boton6'}
               manejarClick={manejarClick} />
           </div>
@@ -76,42 +75,36 @@ function App() {
               id={'boton7'}
               imagen='mira-que-te-como-hermano.jpg'
               audio='mira-que-te-como-hermano.mp3'
-              nombre='Mira que te como hermano'
               estaApretado={selectedButton === 'boton7'}
               manejarClick={manejarClick} />
             <Boton
               id={'boton8'}
               imagen='la-pelota-no-se-mancha.jpg'
               audio='la-pelota-no-se-mancha.mp3'
-              nombre='La pelota no se mancha'
               estaApretado={selectedButton === 'boton8'}
               manejarClick={manejarClick} />
             <Boton
               id={'boton9'}
               imagen='que-la-gente-crea.jpg'
               audio='que-la-gente-crea.mp3'
-              nombre='Que la gente crea'
               estaApretado={selectedButton === 'boton9'}
               manejarClick={manejarClick} />
             <Boton
               id={'boton10'}
               imagen='podes-ser-tan-pelotudo-viejo.jpg'
               audio='podes-ser-tan-pelotudo-viejo.mp3'
-              nombre='Podes ser tan pelotudo viejo'
               estaApretado={selectedButton === 'boton10'}
               manejarClick={manejarClick} />
             <Boton
               id={'boton11'}
               imagen='inventaron-el-var.jpg'
               audio='inventaron-el-var.mp3'
-              nombre='Inventaron el  VAR'
               estaApretado={selectedButton === 'boton11'}
               manejarClick={manejarClick} />
             <Boton
               id={'boton12'}
               imagen='y-va-el-tercero.jpg'
               audio='y-va-el-tercero.mp3'
-              nombre='Y va el tercero gol de river'
               estaApretado={selectedButton === 'boton12'}
               manejarClick={manejarClick} />
 
@@ -123,42 +116,36 @@ function App() {
               id={'boton13'}
               imagen='soy-una-bestia.jpg'
               audio='soy-una-bestia.mp3'
-              nombre='Soy una bestia'
               estaApretado={selectedButton === 'boton13'}
               manejarClick={manejarClick} />
             <Boton
               id={'boton14'}
               imagen='le-pongo-un-10.jpg'
               audio='le-pongo-un-10.mp3'
-              nombre='Le pongo un 10'
               estaApretado={selectedButton === 'boton14'}
               manejarClick={manejarClick} />
             <Boton
               id={'boton15'}
               imagen='no-me-midas.jpg'
               audio='no-me-midas.mp3'
-              nombre='No me midas'
               estaApretado={selectedButton === 'boton15'}
               manejarClick={manejarClick} />
             <Boton
               id={'boton16'}
               imagen='boca-la-concha-de-tu-madre.jpg'
               audio='boca-la-concha-de-tu-madre.mp3'
-              nombre='Boca la concha de tu madre'
               estaApretado={selectedButton === 'boton16'}
               manejarClick={manejarClick} />
             <Boton
               id={'boton17'}
               imagen='estamos-en-la-B.jpg'
               audio='estamos-en-la-B.mp3'
-              nombre='Noooo estamos en la B'
               estaApretado={selectedButton === 'boton17'}
               manejarClick={manejarClick} />
             <Boton
               id={'boton18'}
               imagen='cancion-b-hinchas-belgrano.jpg'
               audio='cancion-b-hinchas-belgrano.mp3'
-              nombre='Cancion de la B por hinchas de Belgrano'
               estaApretado={selectedButton === 'boton18'}
               manejarClick={manejarClick} />
 
--- src/componentes/Boton.js
+++ src/componentes/Boton.js
@@ -1,38 +1,47 @@
-import  React, { useState } from 'react';
+import React, { useState } from 'react';
 import '../hojas-de-estilo/Boton.css';
 import '../hojas-de-estilo/BotonMovil.css';
 
-//Recibo 3 props
 function Boton(props) {
 
   const [audioPlaying, setAudioPlaying] = useState(null);
 
+
   function playAudio(audio) {
+    /* Caso en donde aprieto el mismo boton, si detecta que esta reproduciendo audio al volver a apretar lo que hara es
+    pausar el anterior audio y reproducir el nuevo audio (como es el mismo boton va a ser el mismo audio) */
+    
     if (audioPlaying) {
-      audioPlaying.pause();
+      setAudioPlaying(null);
+      audioPlaying.pause(); //Va a reiniciar el audio cada vez que se aprieta el mismo boton
     }
-    var newAudio = new Audio(require(`../audios/${audio}`));
-    newAudio.play();
+    var newAudio = new Audio(require(`../audios/${audio}`)); //Le paso a la variable newAudio el audio que se va a escuchar
+
+    newAudio.play(); //Se reproduce el audio
     setAudioPlaying(newAudio);
+
+    /* Si el boton esta apretado la clase va a hacer 'boton-contenedor boton-con-click', 
+    cuando termine el audio va a pasar a la clase 'boton-contenedor boton-sin-click'*/
+    
+    newAudio.addEventListener('ended', function () {
+      document.getElementById(props.id).className = 'boton-contenedor boton-sin-click'; //Cambia la clase a 'boton-sin-click al terminar el audio'
+      props.manejarClick(props.estaApretado, false); //Pongo el Props.estaApretado en estado falso al terminar el audio
+      setAudioPlaying(null); // Estado del hook en null al terminar el audio
+    });
   }
 
   return (
     <button
       id={props.id}
       className={`boton-contenedor ${props.estaApretado ? 'boton-con-click' : 'boton-sin-click'}`}
-      onClick={() => {
+      onClick= {() => {
         props.manejarClick(props.id)
-        playAudio(props.audio);
-      }}>
+        playAudio(props.audio)
+      }
+      }>
       <img
         src={require(`../imagenes/${props.imagen}`)}
         alt={`Foto de ${props.imagen}`} />
-      {props.children}
-      <div className='contenedor-texto-boton'>
-        <p className='nombre-boton'>
-          <strong>{props.nombre}</strong>
-        </p>
-      </div>
     </button>
     // if esBotonDeClic = true toma la clase 'boton-sin-click sino toma 'boton-con-click'
   );
